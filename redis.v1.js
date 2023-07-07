32ec9e7efb0ce482fa16150bbe95e46e32333b29
ha danh tuan <tuan.ha@buymed.com>
2023-07-07 22:51:36 +0700

    redis v1

--- redis.v1.js
+++ redis.v1.js
@@ -1,15 +1,40 @@
 const express = require("express")
+const {get,set,setnx,incrby,exists} = require('./model.redis')
 
 const app = express()
 
-app.get('/order', (req, res) => {
-  const time = new Date().getTime()
-  console.log(`Time request:::::${time}`)
+app.get('/order', async (req, res) => {
+
+  const stockNumber = 10;
+
+  const keyName = 'iPhone14'
+
+  const orderNumber = 1;
+
+  const getkey = await exists(keyName);
+
+  if(!getkey) {
+    await setnx(keyName, 0)
+  }
+
+  let soldNumber = await get(keyName)
+  soldNumber = await incrby(keyName, 1)  //buy 1 at the time
+
+  if (soldNumber > stockNumber) {
+    return res.json({
+      status: 'error',
+      message: 'Het hang'
+    })
+  }
+
+  console.log('Sold order number: ', soldNumber)
   return res.json({
-    status: 'success',
-    msg: 'OK',
-    time
+    status: 'OK',
+    message: 'Mua hang thanh cong'
   })
+
+
+
 })
 
 
